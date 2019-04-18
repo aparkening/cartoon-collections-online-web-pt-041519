a4ecafeec2978b87a54f949877abf2e5ca62064c
Aaron Parkening <aaron.parkening@gmail.com>
2019-04-18 18:45:29 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,6 +11,8 @@ def long_planeteer_calls(calls)
 end
 
 def find_the_cheese(snacks)
-  # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
+  snacks.find 
+  
 end
