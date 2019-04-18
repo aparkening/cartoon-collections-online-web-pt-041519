08a3d2e631141a59d5cf8d72360312c7d978362e
Aaron Parkening <aaron.parkening@gmail.com>
2019-04-18 18:37:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,25 +1,5 @@
 def roll_call_dwarves(dwarves)
-
-  dwarves.each { |dwarf| puts ""
-  puts 
-
-describe "Cartoon Collections" do
-  describe "#roll_call_dwarves" do
-    it "prints out the 7 dwarfs in a numbered list" do
-      dwarves = ["Dopey", "Grumpy", "Bashful"]
-
-      output = capture_stdout do
-        roll_call_dwarves(dwarves)
-      end
-
-      # This regex allows for arbitrary characters between
-      # the numbering and the name
-      expect(output).to match(/1.*Dopey/)
-      expect(output).to match(/2.*Grumpy/)
-      expect(output).to match(/3.*Bashful/)
-    end
-  end
-
+  dwarves.each_with_index { |dwarf, index| puts "#{index}. #{dwarf}" }
 end
 
 def summon_captain_planet# code an argument here
